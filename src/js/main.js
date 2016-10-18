17ee9f4036820f7ba7b15192af512414fa0fb670
Samantha Davis <samantha.shea.davis@gmail.com>
2016-10-17 21:49:03 -0400

    added game score, total questions, correct and incorrect questions count

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,39 @@
 //JeopardyGame Constructor
+var game = {
+  qCount: [],
+  incorrectCount: [],
+  correctCount: [],
+  score: 0
+};
+
+
 function Question(response) {
     this.info = {
         question: response[0].question,
-        answer: response[0].answer
+        answer: response[0].answer,
+        value: response[0].value
     };
     this.postQ(this.info);
 }
 
 // Prototype Methods
 
-// 1. Post new question to question field
+// 1. Post new question to question field, clear textarea and checkbox from previous question
 Question.prototype.postQ = function(info) {
+    $('.check').hide();
+    $('.wrong').hide();
     $('.question').html(this.info.question);
+    game.qCount.push(this.info);
+    $('.totalCount').children('span').html(game.qCount.length);
     this.answerQ(info);
+
 };
 
 // 2. Create answer field
 Question.prototype.answerQ = function(info) {
     $('.guessBtn').on('click', function(event) {
         event.preventDefault();
-        var textbox = $('<textarea>').appendTo('.answer');
+        $('textarea').slideDown();
     });
     this.logAnswer(info);
     console.log(info.answer);
@@ -39,7 +53,6 @@ Question.prototype.logAnswer = function(info) {
 
 // 4. Check answer: find longest word in correct answer string, see if user answer includes that word.
 Question.prototype.checkAnswer = function(answer, info) {
-    console.log(info);
     var answerSplit = answer.split(' ');
     var correctAnswerSplit = info.answer.split(' ');
     var longestWord = '';
@@ -48,14 +61,42 @@ Question.prototype.checkAnswer = function(answer, info) {
             longestWord = correctAnswerSplit[count];
         }
     }
-    console.log(longestWord);
     if(answer.includes(longestWord)) {
-      $('.check').slideDown();
+      this.correct(answer, info);
     } else {
-      $('.wrong').slideDown();
+      this.incorrect(answer, info);
     }
+    // getQuestion();
 };
 
+// 5. If answer is correct, add to number of correctly answered questions.
+Question.prototype.correct = function(answer, info) {
+  $('.check').slideDown();
+  // add to correctly answered questions array
+  game.correctCount.push(info);
+  $('.correctCount').children('span').html(game.correctCount.length);
+  // update score
+  game.score += info.value;
+  $('.score').html(game.score);
+  this.clear();
+};
+//6. If answer is incorrect, decrement score, add to incorrect questions count.
+Question.prototype.incorrect = function(answer, info) {
+  $('.wrong').slideDown();
+  game.incorrectCount.push(info);
+  $('.incorrectCount').children('span').html(game.incorrectCount.length);
+  game.score -= info.value;
+  $('.score').html(game.score);
+  this.clear();
+};
+
+Question.prototype.clear= function() {
+window.setTimeout(function() {
+  $('.question').val('');
+  $('textarea').val('');
+  getQuestion();
+}, 2000);
+};
 // Ajax Call
 function getQuestion() {
     $.ajax({
